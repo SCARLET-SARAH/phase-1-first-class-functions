ce12659d64e0ac33aa3f9d6568810e94a4238fa3
scarlet <sarahscarlet641@gmail.com>
2024-03-20 10:36:19 +0300

    create function.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+      callback();
+}
+receivesAFunction[function() {
+     console.log("This is the callback function");
+}];
+function returnsANamedFunction(){
+    return function myNamedFunction(){
+        console.log("This is a named function");
+    };
+}
+
+function returnsAnAnonymousFunction(){
+    return "before all"
+}
